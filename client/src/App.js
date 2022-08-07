9e2d7ff62cd7909692b3b518264524a95801d628
vadim.cvirko <cvi-vadim@yandex.ru>
2022-08-07 17:34:47 +0300

    add

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@
 
 function App() {
   return (
-    <div className="text-2xl text-red-600"></div>
+    <div className="text-2xl  text-red-600">
       <h1>App</h1>
-    </div >
+    </div>
   );
 }
 
